7291fee0ce6f92c02e62505357f8540d7d1af7c2
Hong Xu <hong@topbug.net>
2019-07-29 14:59:54 -0700

    Disallow function variants of in-place operators other than the existing ones.
    gh-metadata: pytorch pytorch 22720 gh/xuhdev/9/head
    ghstack-source-id: 39cd9f4bd5d6e437a53bb99fa94f125aa17699d8

--- torch/csrc/jit/register_special_ops.cpp
+++ torch/csrc/jit/register_special_ops.cpp
@@ -501,7 +501,7 @@ RegisterOperators reg({
           at::Tensor tensor;
           double val;
           pop(stack, tensor, val);
-          push(stack, at::fill_(tensor, val));
+          push(stack, tensor.fill_(val));
           return 0;
         },
         aliasAnalysisFromSchema()),
@@ -513,7 +513,7 @@ RegisterOperators reg({
 
           at::Tensor tensor;
           pop(stack, tensor);
-          push(stack, at::zero_(tensor));
+          push(stack, tensor.zero_());
           return 0;
         },
         aliasAnalysisFromSchema()),
