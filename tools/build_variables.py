7f7c22d6861b0677d1d39cb0881c5b8897a7e5c0
Michael Suo <suo@fb.com>
2019-08-16 13:08:31 -0700

    [jit] move cpp tests to python extension
    Trying to avoid ODR violations in fbcode opt builds. dogsciencing.
    
    This is a remake of https://github.com/pytorch/pytorch/pull/19658
    
    ghstack-source-id: f2ac860578d013ff4eda827acb3a115600fefa5e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/24458

--- tools/build_variables.py
+++ tools/build_variables.py
@@ -117,7 +117,6 @@ libtorch_sources = [
     "torch/csrc/jit/register_special_ops.cpp",
     "torch/csrc/jit/scope.cpp",
     "torch/csrc/jit/script/compiler.cpp",
-    "torch/csrc/api/src/jit.cpp",
     "torch/csrc/jit/script/edit_distance.cpp",
     "torch/csrc/jit/script/logging.cpp",
     "torch/csrc/jit/script/convert_to_ssa.cpp",
@@ -147,7 +146,6 @@ libtorch_sources = [
     "torch/csrc/jit/fuser/cpu/fused_kernel.cpp",
     "torch/csrc/jit/fuser/interface.cpp",
     "torch/csrc/jit/function.cpp",
-    "test/cpp/jit/test.cpp",
 ]
 
 libtorch_cuda_sources = [
@@ -279,6 +277,10 @@ def add_torch_libs():
         "torch/csrc/utils/tensor_numpy.cpp",
         "torch/csrc/utils/tensor_types.cpp",
         "torch/csrc/utils/tuple_parser.cpp",
+        # test.cpp included here to enable running cpp tests from python
+        # runner. Ideally we'd create a separate python extension for it, but
+        # it's too much hassle
+        "test/cpp/jit/test.cpp",
     ]
 
     libtorch_python_cuda_sources = [
